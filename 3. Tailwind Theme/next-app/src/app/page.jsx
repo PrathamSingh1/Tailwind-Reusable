1fcc8796033a8a77bb8ef5ea25fe569f4d01d0ff
PrathamSingh1 <prathamjii36@gmail.com>
2026-02-25 10:02:47 +0000

    Tailwind Declaratives

---
+++ 3. Tailwind Theme/next-app/src/app/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home () {
+  return <div>
+    <h1 className='max-w-6xl mx-auto bg-primary font-pratham text-4xl font-bold'>Home Page</h1>
+  </div>
+}
