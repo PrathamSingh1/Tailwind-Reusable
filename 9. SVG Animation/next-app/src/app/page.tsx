bd5bdfd7931d46e7118abc2c745400caf213175b
PrathamSingh1 <prathamjii36@gmail.com>
2026-02-25 10:07:53 +0000

    SVG Animation

---
+++ 9. SVG Animation/next-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { SVGComponent } from "@/components/svg-component";
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="h-screen bg-gray-50 max-w-4xl mx-auto flex items-center flex-col py-20">
+      <SVGComponent />
+    </main>
+  );
+}
